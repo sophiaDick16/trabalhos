14345f5cd2815a6167651b58dd252079fd719112
Sophia Dick <130375419+sophiaDick16@users.noreply.github.com>
2023-11-27 08:22:35 -0300

    Trabalho da Ana

--- App.js
+++ App.js
@@ -1,14 +1,18 @@
-import React from 'react'
-import { StatusBar } from 'react-native'
+import { StatusBar } from 'react';
+import {StatusBar } from 'react-native';
 
-import { NavigationContainer } from '@react-navigation/native'
-import Routes from './src/routes'
+import { NavigationContainer } from '@react-navigation/native';
+import { Routes } from '@react-navigation/native';
+
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar backgroundColor="#38A69D" barStyle="light-content" />
-      <Routes/>
-    </NavigationContainer>
-  );
+      <NavigationContainer>
+              <StatusBar backgroundColor="#38A69D" barStyle="light-content"/>
+              <Routes/>
+      </NavigationContainer>      
+     
+  )
 }
+
+
